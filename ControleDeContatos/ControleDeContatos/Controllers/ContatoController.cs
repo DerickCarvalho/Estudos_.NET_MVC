5f90f1fd079a07d6e81e07da2330698ace6a9fcd
Darukkyh <de.coding95@gmail.com>
2023-09-18 18:06:40 -0300

    backup-commit

--- ControleDeContatos/ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/ControleDeContatos/Controllers/ContatoController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeContatos.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly IContatoRepositorio _contatoRepositorio;
+        public ContatoController(IContatoRepositorio contatoRepositorio)
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,9 +26,16 @@ namespace ControleDeContatos.Controllers
             return View();
         }
 
-        public IActionResult Apagar()
+        public IActionResult ApagarConfirmacao()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            _contatoRepositorio.Adicionar(contato);
+            return RedirectToAction("Index");
         }
     }
 }
--- ControleDeContatos/ControleDeContatos/Program.cs
+++ ControleDeContatos/ControleDeContatos/Program.cs
@@ -1,4 +1,14 @@
+using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure DataBase.
+var connectionString = builder.Configuration.GetConnectionString("DataBase");
+builder.Services.AddDbContext<BancoContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
